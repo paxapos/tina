25c163eacbb274dff077a47be21bb964189c1f64
timmyturner <64375957+tomasfabbrini@users.noreply.github.com>
2022-06-28 19:48:17 -0300

    Update ia_engine.py

--- ia/ia_engine.py
+++ ia/ia_engine.py
@@ -12,7 +12,7 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
 class IaEngine:
     def train(self):
-       base_dir = 'c:/Users/fabbr/Tina/tina/training/pics' 
+       base_dir = 'Insert "pics" path' 
        train_dir = os.path.join(base_dir, 'train')
        validation_dir = os.path.join(base_dir, 'validation')
 
