5e5b0677db802fa7dc84290472fc8d736969dd04
timmyturner <64375957+tomasfabbrini@users.noreply.github.com>
2022-06-28 15:21:42 -0300

    Update ia_engine.py

--- ia/ia_engine.py
+++ ia/ia_engine.py
@@ -1,11 +1,7 @@
-import os 
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
 class IaEngine:
     def train(self):
-        print('Entrenando')
-        pass
+       print('Entrenando')
+
     def predict(self):
         pass
 
@@ -13,7 +9,18 @@ class IaEngine:
 engine = IaEngine()
 #engine.train()
 
-
+######################################################################################################
+import os 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import scipy
+import numpy as np
+import random
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
 base_dir = 'Insert "pics" path' 
 train_dir = os.path.join(base_dir, 'train')
@@ -41,7 +48,7 @@ ncols = 4
 # Index for iterating over images
 pic_index = 0
 
-# Set up matplotlib fig and size it to fit 4x4 pics
+# Set up matplotlib fig, and size it to fit 4x4 pics
 fig = plt.gcf()
 fig.set_size_inches(ncols * 4, nrows * 4)
 
@@ -58,4 +65,153 @@ for i, img_path in enumerate(next_raw_pix+next_burned_pix):
   img = mpimg.imread(img_path)
   plt.imshow(img)
 
+plt.show()
+
+
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Flatten feature map to a 1-dim tensor so we can add fully connected layers
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+# Create output layer with a single node and sigmoid activation
+output = layers.Dense(1, activation='sigmoid')(x)
+
+# Create model:
+# input = input feature map
+# output = input feature map + stacked convolution/maxpooling layers + fully 
+# connected layer + sigmoid output layer
+model = Model(img_input, output)
+
+model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(learning_rate=0.001),
+              metrics=['acc'])
+
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=10,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 10 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=10,
+        class_mode='binary')
+
+history = model.fit(
+      train_generator,
+      steps_per_epoch=1,  # 40 images = batch_size * steps
+      epochs=4,
+      validation_data=validation_generator,
+      validation_steps=1,  # 10 images = batch_size * steps
+      verbose=2)
+
+# Retrieve a list of accuracy results on training and validation data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and validation data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
+
+plt.show()
+
+# Let's define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model after
+# the first.
+successive_outputs = [layer.output for layer in model.layers[1:]]
+visualization_model = Model(img_input, successive_outputs)
+
+# Let's prepare a random input image of a cat or dog from the training set.
+raw_img_files = [os.path.join(train_raw_dir, f) for f in train_raw_fnames]
+burned_img_files = [os.path.join(train_burned_dir, f) for f in train_burned_fnames]
+img_path = random.choice(raw_img_files + burned_img_files)
+
+img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+# Rescale by 1/255
+x /= 255
+
+# Let's run our image through our network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so can have them as part of our plot
+layer_names = [layer.name for layer in model.layers[1:]]
+
+# Now let's display our representations
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+  if len(feature_map.shape) == 4:
+    # Just do this for the conv / maxpool layers, not the fully-connected layers
+    n_features = feature_map.shape[-1]  # number of features in feature map
+    # The feature map has shape (1, size, size, n_features)
+    size = feature_map.shape[1]
+    # We will tile our images in this matrix
+    display_grid = np.zeros((size, size * n_features))
+    for i in range(n_features):
+      # Postprocess the feature to make it visually palatable
+      x = feature_map[0, :, :, i]
+      x -= x.mean()
+      x /= x.std()
+      x *= 64
+      x += 128
+      x = np.clip(x, 0, 255).astype('uint8')
+      # We'll tile each filter into this big horizontal grid
+      display_grid[:, i * size : (i + 1) * size] = x
+    # Display the grid
+    scale = 20. / n_features
+    plt.figure(figsize=(scale * n_features, scale))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+    
 plt.show()
