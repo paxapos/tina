c7b8a1d000af677e2c3f6ca4e353ad73bf327430
joaaquinhidalgo <103291969+joaaquinhidalgo@users.noreply.github.com>
2022-07-20 12:59:16 -0300

    Update ia.py
    comandos para llamar por consola

--- ia/management/commands/ia.py
+++ ia/management/commands/ia.py
@@ -8,17 +8,32 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('train', nargs='+', type=int)
         parser.add_argument('predict', nargs='+', type=int)
-        pass
+	pass
 
-    def handle(self, *args, **options):
+
+    def HandleTrain(self, *args, **options):
         engine = IaEngine()
+	product = input("¿what do you want to train?")
+	ruta = /tina/training/products 
+	for	product	in ruta :
+		try:
+			engine.train("product")
+		except product.DoesNotExist:
+			raise CommandError('product "%s" does not exist' % ruta)
+		product.opened = False
+		product.save()
 
-<<<<<<< HEAD:ia/management/commands/ia.py
-        engine.train("Milanesas")
-        prediction = engine.predict('C:/Users/fabbr/Tina/tina/training/pics/training/burned/pic_01.jpg')
-=======
-        engine.training_()
-        prediction = engine.predict('Test_Pic')
->>>>>>> 616e66f396dd8aeae856acec5634944d7674bc2c:ia/management/commands/train.py
 
-        self.stdout.write(self.style.SUCCESS('PREDICTION ENDS "%s"' % prediction))
+self.stdout.write(self.style.SUCCESS('Successfully closed train "%s"' % ruta))
+	
+    
+    def HandlePredict(self, *args, **options):
+      img = input("which image do you want to predict?")
+      img_input = img_to_array(img.resize((150, 150)))
+      array = model.predict(img_input)
+      score = array[0]
+
+      return score
+
+
+self.stdout.write(self.style.SUCCESS('PREDICTION ENDS "%s"' % prediction))
