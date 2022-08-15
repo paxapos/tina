0c552bcb9d46b44e521227b1476148764c2335a1
Tomás Fabbrini <64375957+tomasfabbrini@users.noreply.github.com>
2022-08-15 19:15:31 -0300

    Update ia_engine.py

--- ia/ia_engine.py
+++ ia/ia_engine.py
@@ -3,15 +3,20 @@ import os
 import random
 import shutil
 import cv2
-from os import listdir
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from os import listdir
 from pathlib import Path
 from tensorflow.keras import layers
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from PIL import Image as PImage
 from tina.settings import BASE_DIR, TRAINING_PICS_FOLDER, MODEL_PATH, EPOCHS_QUANTITY, VALIDATION_PERCENTAGE
@@ -33,52 +38,30 @@ class IaEngine:
       validation_dir = os.path.join(dirpath, productName, 'validation')
       
       return train_dir, validation_dir
-    
-  
+   
    def __createModel (self):
       '''
       This function creates the structure of the neural network
       and returns the model
       '''
 
-      # Our input feature map is 281x199x3: 281x199 for the image pixels, and 3 for
-      # the three color channels: R, G, and B
-      img_input = layers.Input(shape=(281, 199, 3))
+      model = Sequential()
+      inputShape = (281, 199, 3)
 
-      # First convolution extracts 16 filters that are 3x3
-      # Convolution is followed by max-pooling layer with a 2x2 window
-      x = layers.Conv2D(16, 3, activation='sigmoid')(img_input)
-      x = layers.MaxPooling2D(2)(x)
+      model.add(Conv2D(32, (3, 3), padding="same",
+	      input_shape=inputShape))
+      model.add(Activation("relu"))
+      model.add(Flatten())
+      model.add(Dense(10))
+      model.add(Activation("softmax"))
 
-      # Second convolution extracts 32 filters that are 3x3
-      # Convolution is followed by max-pooling layer with a 2x2 window
-      x = layers.Conv2D(32, 3, activation='sigmoid')(x)
-      x = layers.MaxPooling2D(2)(x)
- 
-      # Third convolution extracts 64 filters that are 3x3
-      # Convolution is followed by max-pooling layer with a 2x2 window
-      x = layers.Conv2D(64, 3, activation='sigmoid')(x)
-      x = layers.MaxPooling2D(2)(x)
-
-      # Flatten feature map to a 1-dim tensor so we can add fully connected layers
-      x = layers.Flatten()(x)
-
-      # Create a fully connected layer with sigmoid activation and 512 hidden units
-      x = layers.Dense(512, activation='sigmoid')(x)
-
-      # Create output layer with a single node and sigmoid activation
-      output = layers.Dense(1, activation='sigmoid')(x)
-
-      # Create model:
-      # input = input feature map
-      # output = input feature map + stacked convolution/maxpooling layers + fully 
-      # connected layer + sigmoid output layer
-      model = Model(img_input, output)
-
-      model.compile(loss='binary_crossentropy',
-        optimizer=RMSprop(learning_rate=0.001),
-        metrics=['acc'])
+      model.compile(
+         optimizer='adam',
+         loss='categorical_crossentropy',
+         metrics=["accuracy"]
+      )
       return model
+
 
    def imageSorter__(self, path: str, productName: str):
       '''
@@ -112,32 +95,28 @@ class IaEngine:
       train_dir, validation_dir = self.__imageReader(productName)
       model = self.__createModel()
      
-      # All images will be rescaled by 1./255
       train_datagen = ImageDataGenerator(rescale=1./255)
       val_datagen = ImageDataGenerator(rescale=1./255)
 
-     # Flow training images in batches of 10 using train_datagen generator
       train_generator = train_datagen.flow_from_directory(
-      train_dir,  # This is the source directory for training images
-      target_size=(281, 199),  # All images will be resized to 199x281
+      train_dir,
+      target_size=(281, 199),
       batch_size=10,
-     # Since we use binary_crossentropy loss, we need binary labels
-      class_mode='binary')
+      class_mode='categorical')
 
-     # Flow validation images in batches of 10 using val_datagen generator
       validation_generator = val_datagen.flow_from_directory(
       validation_dir,
       target_size=(281, 199),
       batch_size=10,
-      class_mode='binary')
+      class_mode='categorical')
 
       print('Training...')
       history = model.fit(
       train_generator,
-      steps_per_epoch=4,  # 40 images = batch_size * steps
+      steps_per_epoch=4,
       epochs=EPOCHS_QUANTITY,
       validation_data=validation_generator,
-      validation_steps=1,  # 10 images = batch_size * steps
+      validation_steps=1,
       verbose=2)
       print('Model Trained!')
 
@@ -158,7 +137,6 @@ class IaEngine:
       image = cv2.imread(img)
       input_array = np.array(np.expand_dims(image, axis=0))
       array = model.predict(input_array)
-      #score = array[0]
       return array
 
    def __accuracyGraph (self):
@@ -167,28 +145,20 @@ class IaEngine:
       for training and validation data 
       based on the amount of epochs 
       '''
-
-      # Retrieve a list of accuracy results on training and validation data
-      # sets for each training epoch
       acc = history.history['acc']
       val_acc = history.history['val_acc']
 
-      # Retrieve a list of list results on training and validation data
-      # sets for each training epoch
       loss = history.history['loss']
       val_loss = history.history['val_loss']
 
-      # Get number of epochs
       epochs = range(len(acc))
 
-      # Plot training and validation accuracy per epoch
       plt.plot(epochs, acc)
       plt.plot(epochs, val_acc)
       plt.title('Training and validation accuracy')
 
       plt.figure()
 
-     # Plot training and validation loss per epoch
       plt.plot(epochs, loss)
       plt.plot(epochs, val_loss)
       plt.title('Training and validation loss')
